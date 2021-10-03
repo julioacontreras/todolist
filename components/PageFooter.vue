7e84f67c6e16618048015234a3dcbb77a6009273
julioacontreras <julio.aranis.contreras@gmail.com>
2021-10-03 15:41:41 +0200

    fix footer text name

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -2,7 +2,7 @@
   <footer class="h-8 absolute clear-both bottom-0 w-full">
     <div class="content has-text-centered">
       <p>
-        TODO List :·)
+        Created by <a href="https://www.linkedin.com/in/julio-contreras-6579b623/" target="BLANK">Julio Contreras</a>  :·)
       </p>
     </div>
   </footer>
