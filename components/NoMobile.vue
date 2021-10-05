a62b970995d0c2a773c6ec2fdaf751b6e9d9d691
julioacontreras <julio.aranis.contreras@gmail.com>
2021-10-05 09:16:11 +0200

    verification to block app when in mobile

---
+++ components/NoMobile.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-lg flex justify-center pt-6">
+    No está disponible para móvil.
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,8 @@
   <div>
     <page-header />
     <section>
-      <nuxt />
+      <nuxt v-if="!isMobile" />
+      <no-mobile v-if="isMobile" />
     </section>
     <page-footer />
   </div>
@@ -11,11 +12,25 @@
 <script>
 import PageHeader from '../components/PageHeader.vue'
 import PageFooter from '../components/PageFooter.vue'
+import NoMobile from '../components/NoMobile.vue'
+
+const MobileDetect = require('mobile-detect')
 
 export default {
   components: {
     PageHeader,
-    PageFooter
+    PageFooter,
+    NoMobile
+  },
+  data () {
+    return {
+      isMobile: false
+    }
+  },
+  created () {
+    // eslint-disable-next-line nuxt/no-globals-in-created
+    const md = new MobileDetect(window.navigator.userAgent)
+    this.isMobile = (md.mobile() !== null)
   }
 }
 </script>
